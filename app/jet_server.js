19e78b1926cd818a197a884bc637752fca50693d
ogadimma osuji <orgmatter@gmail.com>
2021-12-16 23:30:56 +0100

    modifications on proxy for binance api

--- app/jet_server.js
+++ app/jet_server.js
@@ -5,9 +5,18 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = 3000;
 const app = express();
 app.use(cors(), express.static('public'), bodyParser.json());
+
+app.use(
+  '/bapi/c2c/v2/friendly/c2c/adv/search', 
+  createProxyMiddleware({
+    target: "https://p2p.binance.com",
+    changeOrigin: true
+  })
+)
 
 // Send application
 app.get('*', (req, res) => {
